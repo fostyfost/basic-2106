55a95120ddb6806d9d58b547ed4dbcf9a424b65d
Fosty Fost <fostyfost@gmail.com>
2019-07-12 00:09:08 +0300

    HT5: Доработать API, удалить `fixtures`

--- simple_api/api/index.js
+++ simple_api/api/index.js
@@ -1,53 +1,49 @@
-var router = require("express").Router();
-var mocks = require("./mock");
+const router = require("express").Router();
+const mocks = require("./mock");
+
+const getById = (id, mock) => mocks[mock].find(item => item.id === id);
+
+const getByIds = (ids, mock) =>
+  ids.split(",").reduce((acc, id) => {
+    let item = getById(id, mock);
+    if (item) {
+      return [...acc, item];
+    }
+    return acc;
+  }, []);
+
+const get = (query, mock) => {
+  switch (true) {
+    case !!query.id:
+      return getById(query.id, mock);
+
+    case !!query.ids:
+      return getByIds(query.ids, mock);
+
+    default:
+      return mocks[mock];
+  }
+};
 
 const reply = (res, body, timeout = 1000, status = 200) =>
   setTimeout(() => {
     res.status(status).json(body);
   }, timeout);
 
-router.get("/restaurants", function(req, res, next) {
-  reply(res, mocks.restaurants);
+router.get("/restaurants", function(req, res) {
+  reply(res, get(req.query, "restaurants"));
 });
 
-router.get("/dishes", function(req, res, next) {
-  var id = req.query.id;
-  var result = mocks.dishes;
-  if (id) {
-    var restaurant = mocks.restaurants.find(function(r) {
-      return r.id === id;
-    });
-    if (restaurant) {
-      result = restaurant.menu.map(function(dishId) {
-        return mocks.dishes.find(function(dish) {
-          return dish.id === dishId;
-        });
-      });
-    }
-  }
-  reply(res, result);
+router.get("/dishes", function(req, res) {
+  reply(res, get(req.query, "dishes"));
 });
 
-router.get("/reviews", function(req, res, next) {
-  var id = req.query.id;
-  var result = mocks.reviews;
-  if (id) {
-    var restaurant = mocks.restaurants.find(function(r) {
-      return r.id === id;
-    });
-    if (restaurant) {
-      result = restaurant.reviews.map(function(reviewId) {
-        return mocks.reviews.find(function(review) {
-          return review.id === reviewId;
-        });
-      });
-    }
-  }
-  reply(res, result);
+router.get("/reviews", function(req, res) {
+  reply(res, get(req.query, "reviews"));
 });
 
-router.get("/users", function(req, res, next) {
-  reply(res, mocks.users);
+router.get("/users", (req, res) => {
+  reply(res, get(req.query, "users"));
 });
 
 module.exports = router;
